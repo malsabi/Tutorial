32ed3d41132122dcc3f22b4462708e75aa6f42ee
MariamAlnaqbi <61577106+MariamAlnaqbi@users.noreply.github.com>
2022-05-17 22:22:42 +0400

    added mariam was here

--- ClientMain.py
+++ ClientMain.py
@@ -4,3 +4,5 @@ if __name__ == '__main__':  # EntryPoint
     InnerSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     InnerSock.connect(("127.0.0.1", 1669))
     input("Client connected")
+
+#mariam was here
